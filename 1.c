e0791a201b43d16212995ec9129eec5ab92f7453
pmalec1 <01169056@pw.edu.pl>
2022-10-21 16:50:59 +0200

    Pobranie pliku

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[]){
+	int firstNumber=0, secondNumber=0;
+	printf("Enter 2 int numbers\n");
+	scanf("%d,%d",&firstNumber,&secondNumber);
+	printf("First=%d Second=%d\n",firstNumber, secondNumber);
+    return 0;
+}
