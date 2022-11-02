c63912e77bb14d4b3fe9d45946c16db939d67ab5
pmalec1 <01169056@pw.edu.pl>
2022-11-02 23:06:17 +0100

    Utworzenie pliku naglowkowego do biblioteki wlasnych funkcji Plik zawiera definicje funkcji uzywanych przez biblioteke

---
+++ my_library.h
@@ -0,0 +1,4 @@
+// Declarations of my functions
+#define  SIZE_OF_FACT_ARRAY 20
+long int calculate_factorial_function( int factorial_argument);
+long int calc_factorial_from_zero_to_limit(int upperlimit,long int array_of_factorials[upperlimit]) ;
