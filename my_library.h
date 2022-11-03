3194bd1049c0e2bf77c6d2a6c0cd5a86425f4bfa
pmalec1 <01169056@pw.edu.pl>
2022-11-03 23:06:18 +0100

    Replace my_library.h

--- my_library.h
+++ my_library.h
@@ -2,7 +2,7 @@
 #define  SIZE_OF_FACT_ARRAY 20
 
 long int calculate_factorial_function( short factorial_argument);
-long int calc_factorial_from_zero_to_limit(short upperlimit,long int array_of_factorials[upperlimit]) ;
+long int calc_factorial_from_zero_to_limit(const short UPPERLIMIT,long int array_of_factorials[UPPERLIMIT]) ;
 void show_interface(float *xstart, float *step, short *number_of_modifications, short *number_of_components);
 int check_data_from_user(short *number_of_components, short *number_of_modifications, float *xstart, float *step);
 float maclaurin_sinus(float starting_point,short max_step,long int array_of_factorials_0_to_limit[SIZE_OF_FACT_ARRAY] );
