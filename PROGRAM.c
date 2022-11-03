be633a35c64413ac351ad2c0d65eabeb2b238779
pmalec1 <01169056@pw.edu.pl>
2022-11-03 21:16:10 +0100

    Update PROGRAM.c

--- PROGRAM.c
+++ PROGRAM.c
@@ -6,14 +6,14 @@
 
 int main(void)
 {
- short upperlimit = SIZE_OF_FACT_ARRAY;
- long int array_of_factorials_0_to_limit[SIZE_OF_FACT_ARRAY];
- float xstart = 0,step =0;  
- short number_of_modifications = 0, number_of_components = 0;          
+ short upperlimit = SIZE_OF_FACT_ARRAY;                                //{
+ long int array_of_factorials_0_to_limit[SIZE_OF_FACT_ARRAY];          // 
+ float xstart = 0,step =0;                                             //
+ short number_of_modifications = 0, number_of_components = 0;          //
  float *xstart_ptr = &xstart, *step_ptr = &step;                       // declaration of local(main) variables  
- float displaying_xstart;
- short *number_of_components_ptr = &number_of_components;
- short *number_of_modifications_ptr = &number_of_modifications;
+ float displaying_xstart;                                              //
+ short *number_of_components_ptr = &number_of_components;              //
+ short *number_of_modifications_ptr = &number_of_modifications;        //}
  //Calculating factorials and writing results into array
  calc_factorial_from_zero_to_limit(upperlimit,array_of_factorials_0_to_limit); // write factorial of  each number (from 0 to SIZE_OF_FACT_ARRAY) to correct index
  show_interface(xstart_ptr,step_ptr,number_of_modifications_ptr,number_of_components_ptr);// display interface and collect some data from user 
