68f46fc5c81b36430e85a3b303ffa763f170ba4a
pmalec1 <01169056@pw.edu.pl>
2022-11-03 20:02:35 +0100

    Replace my_library.h

--- my_library.h
+++ my_library.h
@@ -1,4 +1,9 @@
 // Declarations of my functions
 #define  SIZE_OF_FACT_ARRAY 20
-long int calculate_factorial_function( int factorial_argument);
-long int calc_factorial_from_zero_to_limit(int upperlimit,long int array_of_factorials[upperlimit]) ;
+
+long int calculate_factorial_function( short factorial_argument);
+long int calc_factorial_from_zero_to_limit(short upperlimit,long int array_of_factorials[upperlimit]) ;
+void show_interface(float *xstart, float *step, short *number_of_modifications, short *number_of_components);
+int check_data_from_user(short *number_of_components, short *number_of_modifications, float *xstart, float *step);
+float maclaurin_sinus(float starting_point,short max_step,long int array_of_factorials_0_to_limit[SIZE_OF_FACT_ARRAY] );
+float maclaurin_cosinus(float starting_point,short max_step,long int array_of_factorials_0_to_limit[SIZE_OF_FACT_ARRAY] );
