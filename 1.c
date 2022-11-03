74fade1a51ebc9378ccd868d8075897f9ff70c74
pmalec1 <01169056@pw.edu.pl>
2022-11-03 20:04:35 +0100

    Uzupelnienie programu o niezbedne funkcjonalnosci. Pelna implementacja algorytmu wraz z weryfikacja danych wejsciowych

--- 1.c
+++ 1.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "my_library.h"
 #include "my_library.c"
 
+
 int main(void)
 {
- int upperlimit = SIZE_OF_FACT_ARRAY;
- long int array_of_factorials_0_to_limit[SIZE_OF_FACT_ARRAY];
- int upperlimit = SIZE_OF_FACT_ARRAY;
+ short upperlimit = SIZE_OF_FACT_ARRAY;
  long int array_of_factorials_0_to_limit[SIZE_OF_FACT_ARRAY];
  float xstart = 0,step =0;  
- int number_of_modifications = 0, number_of_components = 0; 
+ short number_of_modifications = 0, number_of_components = 0; 
  float *xstart_ptr = &xstart, *step_ptr = &step;                       // declaration of local(main) variables  
  float displaying_xstart;
- int *number_of_components_ptr = &number_of_components,*number_of_modifications_ptr =&number_of_modifications;
+ short *number_of_components_ptr = &number_of_components;
+ short *number_of_modifications_ptr = &number_of_modifications;
  //Calculating factorials and writing results into array
  calc_factorial_from_zero_to_limit(upperlimit,array_of_factorials_0_to_limit); // write factorial of  each number (from 0 to SIZE_OF_FACT_ARRAY) to correct index
- return 0;
- }
+ show_interface(xstart_ptr,step_ptr,number_of_modifications_ptr,number_of_components_ptr);// display interface and collect some data from user 
+ int error = check_data_from_user(number_of_components_ptr,number_of_modifications_ptr,xstart_ptr,step_ptr);
+ displaying_xstart = xstart;  
+ for(short i = 0; i<=number_of_modifications; i++ )
+  { 
+    if(error != 0)
+    {
+      printf("\n Input data status = incorrect\n");
+      printf("\n NOTE: INPUT DATA MUST BE NUMBERS!!!!! ");
+      break;
+    }
+    if( i == 0 && error == 0)printf("\ninput data status = correct\n");
+    if(xstart > 2*M_PI) xstart = fmod(xstart, 2*M_PI);
+    printf("sin(%.2f) = %.3f " ,displaying_xstart, maclaurin_sinus(xstart,number_of_components,array_of_factorials_0_to_limit));
+    printf("\t\tcos(%.2f) = %.2f\n",displaying_xstart, maclaurin_cosinus(xstart,number_of_components,array_of_factorials_0_to_limit));
+    xstart += step,displaying_xstart+=step;  
+  }
+return 0;
+}
